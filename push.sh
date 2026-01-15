a3fb0e4f0dda18391f86160d55ef3a7ac40b155b
Hoyant_Su <shy3765@163.com>
2026-01-15 17:52:33 +0000

    RA-CMFormer released.

--- push.sh
+++ push.sh
@@ -4,10 +4,10 @@ git config --global credential.helper store
 cd /inspire/hdd/project/continuinglearning/suhaoyang-240107100018/suhaoyang-240107100018/storage/RA-CMFormer
 
 read -p "Input your GitHub token: " TOKEN
-# git remote add origin "https://${TOKEN}@github.com/Hoyant-Su/RA-CMFormer.git"
-git remote set-url origin "https://${TOKEN}@github.com/Hoyant-Su/RA-CMFormer.git"
+git remote add origin "https://${TOKEN}@github.com/Hoyant-Su/RA-CMFormer.git"
+# git remote set-url origin "https://${TOKEN}@github.com/Hoyant-Su/RA-CMFormer.git"
 
 git add .
 git commit -m "RA-CMFormer released."
-# git checkout -b main ## the first time when intializing the repo link
-git push origin main
+git checkout -b main ## the first time when intializing the repo link
+# git push origin main
